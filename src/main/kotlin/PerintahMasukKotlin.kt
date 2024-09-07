dc02cf994b9ebd5aa8cd4e8ed7fcc85fd786f044
ERMA-YUNI <eyuni0464@gmail.com>
2024-09-07 11:42:08 +0700

    add PerintahMasukKotlin.kt

---
+++ src/main/kotlin/PerintahMasukKotlin.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+   println("hI i'M lORA, the virtual parrot,and i Love to repeat!");
+    println("Type something in:");
+    var input:String
+    input= readLine()!!
+    var output: String
+    output = input + "," + input +"!"
+    println(output)
+}
---
+++ src/main/kotlin/ProgramsandiMorse.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    val s= ".. -.-.-...--- -.-. .. .- .-.."
+    println("The original message:s")
+
+    var message = ""
+
+    val alphabetChars = "abcdefghijklmnopqrstuvwxyz"
+    val morseChars = arrayOf(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..")
+ val characters = s.split("")
+    for (morseChar in characters) {
+        val index = morseChars.indexOf(morseChar)
+        if (index != -1) {
+            message += alphabetChars[index]
+        }
+    }
+    println("the decoded mesage: $message")
+        }
